8c376a5aee844905e8509becc16d4b4c2b655305
Hyunjae Woo <hjwoo@umich.edu>
2023-03-05 10:55:04 -0500

    Parse resume and extract text

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from tika import parser
 from flask import Flask, request
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'data')
+UPLOAD_FOLDER = 'data'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app = Flask(__name__)
@@ -12,30 +12,26 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 # Define a function to extract text and metadata from a PDF file using the Tika parser
-def extract_pdf_data(file):
-    parsed_pdf = parser.from_file(file)
+def extract_pdf_data(filename):
+    parsed_pdf = parser.from_file(filename)
     return parsed_pdf['content'], parsed_pdf['metadata']
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    print(f'request method: {request.method}')
-    print(f'request object: {request}')
-    # print(f'request files: {request.files}')
-    # print(f'request form: {request.form}')
-
     if 'file' in request.files:
         file = request.files['file']
         print(f'request file: {file}')
         print(f'filename: {file.filename}')
-        # print(f'file read: {file.read().decode("latin-1")}')
-        print(f'folder: {app.config["UPLOAD_FOLDER"]}')
 
         # Save the uploaded file to local dir
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        filename = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        file.save(filename)
 
-        # text, metadata = extract_pdf_data(file)
+        text, metadata = extract_pdf_data(filename)
         # Do something with the extracted text and metadata
+        print(f'parsed metadata: {metadata}')
+        print(f'parsed text: {text}')
         return 'PDF file uploaded successfully'
     else:
         print(f'No file')
